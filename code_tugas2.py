8cb87e79214bc80bb93843cc70d10b7bdb2627cd
NurdilaAsrin016 <56118281+NurdilaAsrin016@users.noreply.github.com>
2019-10-03 23:12:42 +0900

    tugas 2 pemograman game

---
+++ code_tugas2.py
@@ -0,0 +1,7 @@
+# Tugas Ke2. MK Pemrograman Game
+
+print("Halo nama saya nurdila asrin")
+print("Halo Teknik Info UMMU Ternate")
+#baris ini tidak akan dieksekusi
+print ("Halo Anak Info....") # baris ini dieksekui
+#Baris ini juga merupakan Baris Komentar
